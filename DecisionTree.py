c966699c8467271afb5b0c44110afa4b66c0b82d
tflave <franknguyen007@gmail.com>
2016-02-29 16:20:05 +0700

    Change way of experiments

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
+from random import shuffle,randint,choice
+import numpy.random as nprnd
 import pandas as pd
+from sklearn.cross_validation import train_test_split
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from Tree import DecisionTree as dt
+import copy
 
 
 
@@ -16,9 +20,9 @@ def split(df, k):
 
 k = 10
 
-df = pd.read_csv('Vote_Processed.csv')
+df = pd.read_csv('Car_Processed.csv')
 
-df = split(df, k)
+#df = split(df, k)
 
 # for row in df[0].iterrows():
 #     index, data = row
@@ -27,28 +31,59 @@ df = split(df, k)
 
 remt_tree = dt()
 cart_tree = dt()
-count = []
-count2 = []
-for i in range(k):
-    train_set = pd.concat(df[:i] + df[i+1:])
-    test_set = df[i]
-    print('start REMT')
-    remt_tree.fit(train_set)
-    train_set = train_set.drop(['temp'], axis = 1)
-    count.append(remt_tree.test(test_set))
-    print('start CART')
-    cart_tree.fit(train_set, 'CART')
-    count2.append(cart_tree.test(test_set))
+count_remt = []
+count_cart = []
 
-    print i
+no_sample = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+no_class = 4
+test_set = df.copy(deep=True)
+for i in no_sample:
+    sum_remt = 0
+    sum_cart = 0
+    for j in range(k):
+        train_label = nprnd.randint(no_class, size = i)
+        while len(set(train_label))<no_class:
+            train_label = nprnd.randint(no_class, size = i)
+        train_set = pd.DataFrame(columns=test_set.columns)
+        for label in train_label:
+            row_indexes = test_set[test_set['CLASS'] == label].index.tolist()
+            idx = choice(row_indexes)
+            train_set = train_set.append(test_set.loc[idx], ignore_index=True)
+            test_set.drop(idx)
+        
+        remt_tree.fit(train_set)
+        count = remt_tree.test(test_set)
+        cart_tree.fit(train_set)
+        count2 = cart_tree.test(test_set)
+        count count2
+    #for i in range(k):
+    #print sum(count[i]), float(sum(count[i]))/len(count[i]), sum(count2[i]), float(sum(count2[i]))/len(count2[i])
 
-pca = PCA(n_components=3)
-n = df[1].shape[0]
-df_draw = df[1].copy()
-df_draw = df_draw.drop(['CLASS'], axis = 1)
-X_R = pca.fit_transform(df_draw)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+
+
+
+#for i in range(k):
+    # train_set = pd.concat(df[:i] + df[i+1:])
+    # test_set = df[i]
+    # print('start REMT')
+    # remt_tree.fit(train_set)
+    # train_set = train_set.drop(['temp'], axis = 1)
+    # count.append(remt_tree.test(test_set))
+    # print('start CART')
+    # cart_tree.fit(train_set, 'CART')
+    # count2.append(cart_tree.test(test_set))
+    #
+    # print i
+
+
+
+# pca = PCA(n_components=3)
+# n = df[1].shape[0]
+# df_draw = df[1].copy()
+# df_draw = df_draw.drop(['CLASS'], axis = 1)
+# X_R = pca.fit_transform(df_draw)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 # for i in range(n):
 #     if count[i] == 1 and count2[i] == 1:
 #         ax.scatter(X_R[i][0], X_R[i][1], X_R[i][2], c='#1AA130')
@@ -60,7 +95,3 @@ ax = fig.add_subplot(111, projection='3d')
 #         ax.scatter(X_R[i][0], X_R[i][1], X_R[i][2], c='k')
 
 #plt.show()
-print count2
-for i in range(k):
-    print sum(count[i]), float(sum(count[i]))/len(count[i]), sum(count2[i]), float(sum(count2[i]))/len(count2[i])
-
